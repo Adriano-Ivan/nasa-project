7b0fa8518a47e3c82ea66bfa0de74f16d3fa2eb0
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-02-22 21:27:07 -0300

    Automatizando e antecipando o deploy

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const planetsRouter = require("./routes/planets/planets.router");
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/planets", planetsRouter);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 module.exports = app;
