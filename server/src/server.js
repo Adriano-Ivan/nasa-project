1f71b6ab5586b7096ab6dba6b7972b4e3f9c8c90
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-03-06 15:10:17 -0300

    Inserindo dados da API SpaceX e introduzindo paginação.

--- server/src/server.js
+++ server/src/server.js
@@ -7,15 +7,15 @@ cluster.schedulingPolicy = cluster.SCHED_RR;
 dotenv.config();
 
 const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 async function startServer() {
   await mongoConnect();
-
   await loadPlanetsData();
-
+  await loadLaunchData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
